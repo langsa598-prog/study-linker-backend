99e15a5b7152aa787f64bc2ccc37e0beaae96c4f
민지 <langsa598@gmail.com>
2025-09-27 21:56:22 +0900

    도메인 설정 연결 확인 수정본

--- src/main/java/com/study/service/config/WebConfig.java
+++ src/main/java/com/study/service/config/WebConfig.java
@@ -9,7 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns(
+                        "http://localhost:3000",          // 로컬 개발용 React
+                        "http://gachon.studylink.click",  // 배포된 프론트
+                        "https://gachon.studylink.click"  // HTTPS 대비
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
